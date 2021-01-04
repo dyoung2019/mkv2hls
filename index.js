4351e2d390b742b2d9f4f9668eb6035ed7eba4b8
David Young <david.young.email@gmail.com>
2021-01-04 17:23:53 +1100

    first instance of subs extract

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node 
 
-const createFolderIfNeeded = require('./src/createFolderIfNeeded')
 const prepareArguments = require('./src/prepareArguments')
-const extractOutputFolder = require('./src/extractOutputFolder')
-const inspectMkvFile = require('./src/inspectMkvFile')
-const videoCommand = require('./src/videoCommand')
-const extractVideoInfo = require('./src/extractVideoInfo')
-const extractSubtitleInfo = require('./src/extractSubtitlesInfo')
-const extractAudioTracks = require('./src/extractAudioTracks')
+const prepareOutputFolder = require('./src/prepareOutputFolder')
+
+// const videoCommand = require('./src/videoCommand')
+const analyseMkvContainer = require('./src/analyseMkvContainer')
 
 const argv = prepareArguments(process.argv.slice(2))
 
 const inputFile = argv.input  
 
 // prepare output folder
-const outputFolder = extractOutputFolder(inputFile, argv)
-createFolderIfNeeded(outputFolder)
+
+const outputFolder = prepareOutputFolder(inputFile, argv)
 
 const renditions = require('./src/renditions')
 const buildPlaylist = require('./src/buildPlaylist')
+
+const openShortLivedFolder = require('./src/openShortLivedFolder')
+const extractEachSubtitle = require('./src/extractEachSubtitle')
 
 // for each rendition
 const currentRendition = renditions[2]
@@ -35,21 +35,8 @@ const params = {
   subtitleIndex: 1,
 }
 
-let videoInfo = null
-//  Analyzes mkv container file for tracks and subtitles
-inspectMkvFile(inputFile)
-  // set video info
-  .then(data => {
-    // console.log(data)
-    videoInfo = {
-      info: extractVideoInfo(data),
-      subtitles: extractSubtitleInfo(data),
-      tracks: extractAudioTracks(data)
-    }
-    console.log(data.tracks)
-    // console.log('audio', videoInfo.tracks)
-    // console.log('subs', videoInfo.subtitles)
-  })
+// let videoInfo = null
+analyseMkvContainer(inputFile, outputFolder)
   // extract extra audio tracks
   // .then(() => {
   //   // console.log(data)
@@ -60,12 +47,33 @@ inspectMkvFile(inputFile)
   //   // console.log(data)
   //   return videoCommand.run(params)
   // })
-  // .then(() => {
-  //   console.log('on to subs')
-  //   return videoCommand.extract(params)
-  // })
+  .then(videoInfo => {
+    console.log('on to subs')
+    
+    const videoParams = {
+      inputFile,
+      outputFolder,
+      placeHolder: '%05d', 
+    }
+    
+    const tasks = videoInfo.subtitles.map(sub => {
+      const extractInfo = {
+        video: videoParams,
+        track: sub,
+      }
+
+      return openShortLivedFolder('subs-', extractEachSubtitle, extractInfo)
+    })
+
+    return Promise.all(tasks)
+      .then(() => {
+        return videoInfo
+      })
+    // return videoInfo
+  })
   // Write master playlist
-  .then(() => {
+  .then((videoInfo) => {
+    console.log(videoInfo.subtitles)
     console.log('writing master playlist')
     const entries = buildPlaylist(videoInfo)
     console.log(entries)
--- pullDemo.js
+++ pullDemo.js
@@ -4,18 +4,19 @@ const extractEachSubtitle = require('./src/extractEachSubtitle')
 const openShortLivedFolder = require('./src/openShortLivedFolder')
 // create then cleanup files
 
-const subtitleInfo = {
-  inputFile: "test5.mkv",
-  outputFolder: 'video/',
-  subtitlesPath: 'video/subtitles/',
-  // DECIMAL PLACEHOLDER
-  placeHolder: '%05d',
-  audioTrack: 0,
-  subtitleIndex: 1,
-  languageCode: 'hun',
+const extractInfo = {
+  video: {
+    inputFile: "test5.mkv",
+    outputFolder: 'video/',
+    placeHolder: '%05d', 
+  },
+  track: {
+    subtitleIndex: 1,
+    subtitleFolder: 'subs/1_eng'
+  }
 }
 
-openShortLivedFolder('subs-', extractEachSubtitle, subtitleInfo)
+openShortLivedFolder('subs-', extractEachSubtitle, extractInfo)
   .then((output) => {
     console.log('DEL done', output)
   })
